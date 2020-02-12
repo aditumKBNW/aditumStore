84ebe87a2c5b8f87f8aaea0cfaae2b9233aa16d9
Kelvin Cuesta <kelvinscuesta@gmail.com>
2020-02-12 11:43:48 -0500

    Boilerplate setup

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './Components/App.jsx';
+
+
+render(
+  <App />,
+  document.getElementById('root')
+);
---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+
+app.use('/build', (req, res) => res.sendFile((path.resolve(__dirname, '../build/bundle.js'))));
+
+app.use((req, res) => res.sendFile(path.resolve(__dirname, "index.html")));
+
+
+app.listen(PORT, () => console.log(`SERVER LISTENING ON ${PORT}`));
