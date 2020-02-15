7196caca389ec094c25a7292fce67b0afd4745d9
Nicole Abramowski <nabramow@gmail.com>
2020-02-15 16:16:39 -0500

    Make Accessbar only appear on tab press

--- client/Components/Home.jsx
+++ client/Components/Home.jsx
@@ -3,21 +3,54 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './Nav.jsx'
 import About from './About.jsx';
 import Store from './Store.jsx';
-// import Review from './Review.jsx'
-
+import AccessBar from './AccessBar.jsx';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showAccessBar: false,
+    }
+    this.handleKeydown = this.handleKeydown.bind(this);
+  }
+  
+  handleKeydown(event) {
+    if (event.key === 'Tab') {
+      if (this.state.showAccessBar === false) {
+        this.setState({ showAccessBar: true })
+      }
+      if (this.state.showAccessBar === true) {
+        this.setState({ showAccessBar: false })
+      }
+    }
+    
+  }
+
   render() {
+    console.log('this.state ', this.state);
+
+    const showAccessBar = this.state.showAccessBar;
+    let accessBar = null;
+
+    if (showAccessBar) {
+      accessBar = <AccessBar onClick={ this.handleKeydown }/>;
+    } else {
+      accessBar = null;
+    }
+
     return (
-      <Router>
-        <Nav />
-        <Switch >
-          <Route exact path="/about" component={About}/>
-          <Route exact path ='/store' component={Store}/>
-        </Switch>
-        {/* 
-        <Route exact path ='/review' component={Review}/> */}
-      </Router>
+      <div onKeyDown={ this.handleKeydown }>
+        <Router>
+          { accessBar }
+          <Nav />
+          <Switch >
+            <Route exact path="/about" component={About}/>
+            <Route exact path ='/store' component={Store}/>
+          </Switch>
+          {/* 
+          <Route exact path ='/review' component={Review}/> */}
+        </Router>
+        </div>
     );
   }
 }
