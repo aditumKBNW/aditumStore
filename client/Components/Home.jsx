438aa38f49946a65ac3c44e6bf48ac2e72c327ad
Nicole Abramowski <nabramow@gmail.com>
2020-02-19 10:37:26 -0500

    Adjust AccessBar path

--- client/Components/Home.jsx
+++ client/Components/Home.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './Nav.jsx'
 import About from './About.jsx';
 import Store from './Store.jsx';
-import AccessBar from './AccessBar';
+import AccessBar from './AccessBar.jsx';
 import MainContainer from './MainContainer.jsx';
 // import Review from './Review.jsx'
 
