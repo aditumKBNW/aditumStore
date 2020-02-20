78970d75c616fea06cbf8c236735e39fdce183af
johnsben002 <johnsben002@gmail.com>
2020-02-19 19:39:52 -0500

    access bar functionality working

--- client/Components/AccessBar.jsx
+++ client/Components/AccessBar.jsx
@@ -15,21 +15,57 @@ const barStyle =  {
   backgroundColor: 'gray',
 };
 
+// ['sidebar', 'main-content', 'photo-sb', 'footer']
+
+
 export default class AccessBar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      sections: null,
+      currentSelection: null,
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.myRef = React.createRef();
+  }
+
+  handleChange(e) {
+    let currentId = this.state.sections[e];
+    let currentElement = document.querySelector(`#${currentId}`);
+    currentElement.tabIndex = -1;
+    this.myRef.current = currentElement;
+    this.setState({
+      currentSelection: currentId,
+    })
+    this.myRef.current.focus();
+  };
+
+  componentDidMount() {
+    this.setState({
+      sections: this.props.sections,
+    })
+  }
+
   render() {
+
+    // console.log(this.props)
+    const keys = Object.keys(this.props.sections);
+    const options = [];
+    for (let i = 0; i < keys.length; i++) {
+      options.push({ value: keys[i]});
+    }
+
     return (
       <div className ='ally-nav-area' style={ barStyle }>
         <label htmlFor='accessibility-nav-bar'> Jump to: </label>
         <div id='accessibility-nav-bar'>
           <Dropdown
-            options={[
-              { value: 'one' },
-              { value: 'two' },
-              { value: 'three' }
-            ]}
+            options={options}
             style={styles}
             placeholder='Sections of this page'
             ariaLabel='Navigation Assistant'
+            setSelected={this.handleChange}
           />
         </div>
       </div>
